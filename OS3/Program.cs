f482272c8ecd5fac8d685fe6b34445c88c24fb6d
albinosikm <albinosikm@gmail.com>
2020-04-17 11:44:13 +0400

    Добавьте файлы проекта.

---
+++ OS3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OS3
+{
+    class Program
+    {
+        static Random rand = new Random((int)DateTime.Now.Ticks);
+        static void Main(string[] args)
+        {
+            OperatingSystem operatingSystem = new OperatingSystem(256, 64);
+            for (int processId = 0; processId < rand.Next(1,5); processId++)
+            {
+                operatingSystem.addProcess();
+                for (int pageId = 0; pageId < operatingSystem.getProcess(processId).getCountPages(); pageId++)
+                {
+                    operatingSystem.addPage(processId);
+                }
+                operatingSystem.printMemory();
+            }
+            operatingSystem.printMemory();
+            Console.ReadKey();
+        }
+    }
+}
